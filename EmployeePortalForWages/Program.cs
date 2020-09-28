93c0422eb790c378b4a8cdb5a5a43c44c3d08042
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 17:12:01 +0530

    [Tirupathi] Add. Employee monthly wage computation

--- EmployeePortalForWages/Program.cs
+++ EmployeePortalForWages/Program.cs
@@ -9,32 +9,38 @@ namespace EmployeePortalForWages
         public const int FULL_TIME = 1;
         public const int PART_TIME = 0;
 
-        public static Double calculateDailyWage(Double fullDayHrs,Double wagePerHour)
+        public static Double calculateDailyWage(Double workingHrs,Double wagePerHour)
         {
-            return fullDayHrs * wagePerHour;
+            return workingHrs * wagePerHour;
         }
+
+        public static Double calculateMonthlyWage(Double workingHrs,Double wagePerHour,Double workingDaysPerMonth)
+        {
+            return workingHrs * wagePerHour * workingDaysPerMonth;
+        }
+
         static void Main(string[] args)
         {
 
-            int fullDayHrs = 10;
+            int workingHrs = 10;
             int wagePerHour = 20;
+            int workingDaysPerMonth = 20;
 
             Random random = new Random();
             int empType = random.Next(0, 2);
 
             switch (empType) {
                 case PART_TIME :
-                    fullDayHrs = 8;
+                    workingHrs = 8;
                     wagePerHour = 15;
+                    workingDaysPerMonth = 10;
                     Console.WriteLine("Part Time Employee Details ");
                     break;
                 default:
                     Console.WriteLine("Full Time Employee Details ");
                     break;
             }
-
-
-            Console.WriteLine("Daily Wage : " + calculateDailyWage(fullDayHrs, wagePerHour));
+            Console.WriteLine("Monthly Wage : " + calculateMonthlyWage(workingHrs, wagePerHour,workingDaysPerMonth));
         }
     }
 }
