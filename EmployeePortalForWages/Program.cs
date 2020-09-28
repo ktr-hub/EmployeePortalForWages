e553f28825f46e098c6bf362b94f45b12817b38d
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 14:45:25 +0530

    [Tirupathi] Add. Welcome Message in Master

---
+++ EmployeePortalForWages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePortalForWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Portal for Wages!");
+        }
+    }
+}
