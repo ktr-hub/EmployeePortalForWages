e83dd16595e1cf2357307eb6979e0e2fd98ac781
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 17:40:41 +0530

    [Tirupathi] Add. Wage till a Condition satified in UC6

--- EmployeePortalForWages/Program.cs
+++ EmployeePortalForWages/Program.cs
@@ -9,38 +9,42 @@ namespace EmployeePortalForWages
         public const int FULL_TIME = 1;
         public const int PART_TIME = 0;
 
-        public static Double calculateDailyWage(Double workingHrs,Double wagePerHour)
+        public static Double calculateWage(Double workingHrsPerDay, Double maxWorkingHrs,Double maxWorkingDays)
         {
-            return workingHrs * wagePerHour;
-        }
-
-        public static Double calculateMonthlyWage(Double workingHrs,Double wagePerHour,Double workingDaysPerMonth)
-        {
-            return workingHrs * wagePerHour * workingDaysPerMonth;
+            Double workingHrs = 0;
+            Double workingDays = 0;
+            Double RATE_PER_HOUR = 100;
+            while (workingHrs < maxWorkingDays && workingDays < maxWorkingDays)
+            {
+                workingHrs = workingHrs + workingHrsPerDay;
+                workingDays++;
+            }
+            return workingDays * workingHrsPerDay * RATE_PER_HOUR;
         }
 
         static void Main(string[] args)
         {
 
-            int workingHrs = 10;
-            int wagePerHour = 20;
-            int workingDaysPerMonth = 20;
+            int maxWorkingHrs = 100;
+            int workingHrsPerDay = 10;
+            int maxWorkingDays = 20;
 
             Random random = new Random();
             int empType = random.Next(0, 2);
 
             switch (empType) {
                 case PART_TIME :
-                    workingHrs = 8;
-                    wagePerHour = 15;
-                    workingDaysPerMonth = 10;
+                    maxWorkingHrs = 80;
+                    workingHrsPerDay = 8;
+                    maxWorkingDays = 15;
                     Console.WriteLine("Part Time Employee Details ");
                     break;
                 default:
                     Console.WriteLine("Full Time Employee Details ");
                     break;
             }
-            Console.WriteLine("Monthly Wage : " + calculateMonthlyWage(workingHrs, wagePerHour,workingDaysPerMonth));
+
+            Console.WriteLine("Total Wage till the condition reached : " + calculateWage(workingHrsPerDay,maxWorkingHrs,maxWorkingDays));
         }
     }
 }
