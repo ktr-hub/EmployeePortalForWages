efad7811e865af09ced5ad25bfb5f0bd5e7f157e
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 15:15:51 +0530

    [Tirupathi] Add. Attendance Check using Random in UC1

--- EmployeePortalForWages/Program.cs
+++ EmployeePortalForWages/Program.cs
@@ -4,9 +4,20 @@ namespace EmployeePortalForWages
 {
     class Program
     {
+        public static Boolean isEmployeePresent()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 1)
+                return true;
+            return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Portal for Wages!");
+            if (isEmployeePresent())
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is not present");
         }
     }
 }
