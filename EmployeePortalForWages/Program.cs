7f8b03ea7f683171fc9a87f907f59ed6597a0365
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 16:09:50 +0530

    [Tirupathi] Add. Part Time Employee details in UC3 branch

--- EmployeePortalForWages/Program.cs
+++ EmployeePortalForWages/Program.cs
@@ -2,15 +2,38 @@
 
 namespace EmployeePortalForWages
 {
+
     class Program
     {
+
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 0;
+
         public static Double calculateDailyWage(Double fullDayHrs,Double wagePerHour)
         {
             return fullDayHrs * wagePerHour;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Daily Wage : " + calculateDailyWage(8, 20));
+
+            int fullDayHrs = 10;
+            int wagePerHour = 20;
+
+            Random random = new Random();
+            int empType = random.Next(0, 2);
+
+            if (empType==PART_TIME)
+            {
+                fullDayHrs = 8;
+                wagePerHour = 15;
+                Console.WriteLine("Part Time Employee Details ");
+            }
+            else
+            {
+                Console.WriteLine("Full Time Employee Details ");
+            }
+
+            Console.WriteLine("Daily Wage : " + calculateDailyWage(fullDayHrs, wagePerHour));
         }
     }
 }
