175228e2c0a37b9a396216ae0bebc56bad8a2b44
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-30 08:07:43 +0530

    [Tirupathi] Add. created a company List UC12

--- EmployeePortalForWages/EmpWageBuilder..cs
+++ EmployeePortalForWages/EmpWageBuilder..cs
@@ -6,10 +6,13 @@ namespace EmployeePortalForWages
 {
     class EmpWageBuilder:ComputeEmpWage
     {
+        List<Company> companyList;
         Dictionary<string, Double> companySalary;
         public void addCompanyWage(String company, Double empRatePerHour, Double numOfWorkingDays, Double workingHrs)
         {
             Company companyObject = new Company(company, empRatePerHour,  numOfWorkingDays, workingHrs);
+            companyList = new List<Company>();
+            companyList.Add(companyObject);
             companySalary = new Dictionary<string, double>();
             companySalary[company] = empRatePerHour * numOfWorkingDays * workingHrs;
         }
